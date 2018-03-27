cb29ee6eefdcf8277b133fcace71979285699e58
rayraib <binitarai11@gmail.com>
2018-03-27 21:40:12 +0000

    remove duplicate password column

--- models/user.py
+++ models/user.py
@@ -15,7 +15,6 @@ class User(BaseModel, Base):
 
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
-    password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=True)
     last_name = Column(String(128), nullable=True)
 
