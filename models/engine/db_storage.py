011b08b097f81aab223a3b0d7fdbe8201e38693f
Dave Yoon <daveyoon64@gmail.com>
2018-03-27 03:43:11 +0000

    Command all now works for DBStorage

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -8,6 +8,10 @@ from sqlalchemy.ext.declarative import declarative_base
 from models.base_model import Base
 from models.state import State
 from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
+from models.user import User
 import os
 
 
@@ -41,20 +45,23 @@ class DBStorage:
         '''
             query current database session for all objects depending on cls.
         '''
+        match = {}
         Session = sessionmaker(bind=self.__engine)
         session = Session()
-        if cls is None:
-            result = session.query(User, State, City, Amentiy,
-                                   Place, Review).all()
+        if not cls:
+            result = session.query(State, City).all()
+            for element in result:
+                for obj in element:
+                    key = "{}.{}".format(type(obj), obj.id)
+                    match[key] = obj
         else:
             if cls == 'State':
                 result = session.query(State).all()
             elif cls == 'City':
                 result = session.query(City).all()
-        match = {}
-        for element in result:
-            key = "{}.{}".format(type(element), element.id)
-            match[key] = element
+            for element in result:
+                key = "{}.{}".format(type(element), element.id)
+                match[key] = element
         return match
 
     def new(self, obj):
