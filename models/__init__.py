de50f455c3d305b87ca6e57fffc0987d66e1763b
rayraib <binitarai11@gmail.com>
2018-03-28 21:28:08 +0000

    call reload once for both storage

--- models/__init__.py
+++ models/__init__.py
@@ -20,8 +20,7 @@ classes = {"User": User, "BaseModel": BaseModel,
 if os.environ['HBNB_TYPE_STORAGE'] == 'db':
     from models.engine.db_storage import DBStorage
     storage = DBStorage()
-    storage.reload()
 else:
     from models.engine.file_storage import FileStorage
     storage = FileStorage()
-    storage.reload()
+storage.reload()
