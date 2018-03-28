65dba9b0ffa0dcb518eb655b62d318cbf667b03c
rayraib <binitarai11@gmail.com>
2018-03-28 22:56:35 +0000

    use os.environ.get istead of os.environ

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -32,7 +32,7 @@ class DBStorage:
                                           os.environ['HBNB_MYSQL_HOST'],
                                           os.environ['HBNB_MYSQL_DB']),
                                       pool_pre_ping=True)
-        if os.environ['HBNB_ENV'] == 'test':
+        if os.environ.get('HBNB_ENV') == 'test':
             Base.metadata.drop_all(self.__engine)
 
     def all(self, cls=None):
