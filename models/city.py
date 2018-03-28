7e4bed2c7d204c6f4bc9634a51d01e8dc16b60c0
Dave Yoon <daveyoon64@gmail.com>
2018-03-28 06:17:55 +0000

    Swap out storage with models.storage

--- models/city.py
+++ models/city.py
@@ -2,8 +2,9 @@
 '''
     Define the class City.
 '''
-from sqlalchemy import String, Column, ForeignKey
+import os
 from models.base_model import BaseModel, Base
+from sqlalchemy import String, Column, ForeignKey
 from sqlalchemy.orm import relationship
 
 
@@ -12,9 +13,13 @@ class City(BaseModel, Base):
         Define the class City that inherits from BaseModel.
     '''
     __tablename__ = "cities"
-    name = Column(String(128), nullable=False)
-    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
 
-    # places = relationship('Place', backref='cities',
-    #                      cascade='all, delete, delete-orphan')
-    places = relationship('Place', backref='cities', cascade='delete')
+    if os.environ['HBNB_TYPE_STORAGE'] == 'db':
+        name = Column(String(128), nullable=False)
+        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+        # places = relationship('Place', backref='cities',
+        #                      cascade='all, delete, delete-orphan')
+        places = relationship('Place', backref='cities', cascade='delete')
+    else:
+        name = ""
+        state_id = ""
