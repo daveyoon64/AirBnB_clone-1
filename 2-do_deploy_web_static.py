fd00745160e53846399758f9c7df70ee4ffc9cb4
Dave Yoon <daveyoon64@gmail.com>
2018-04-03 22:50:12 +0000

    Make more changes in hope of getting more checks

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """deploy archive for static deployment"""
 from fabric.api import *
-import tarfile
 import os
 
 env.hosts = ['52.204.227.199', '34.207.234.197']
@@ -24,19 +23,18 @@ def do_deploy(archive_path):
         # extract the file
         folder = archive_path[archive_path.find('/')+1:archive_path.find('.')]
         whereto = '/data/web_static/releases/{}'.format(folder)
-        run('sudo mkdir -p /data/web_static/releases/{}'.format(folder))
-        run('sudo tar -xzf {} -C {}'.format(serverpath, whereto))
+        run('mkdir -p /data/web_static/releases/{}'.format(folder))
+        run('tar -xzf {} -C {}'.format(serverpath, whereto))
 
         # delete archive, move files from folder/web_static to folder, delete
-        run('sudo rm -f {}'.format(serverpath))
-        run('sudo mv -u {}/web_static/* {}'.format(whereto, whereto))
-        run('sudo rm -rf {}/web_static'.format(whereto))
+        run('rm -rf {}'.format(serverpath))
+        run('mv -n {}/web_static/* {}'.format(whereto, whereto))
+        run('rm -rf {}/web_static'.format(whereto))
 
         # delete the symlink and create a new one
-        run('sudo rm -rf /data/web_static/current')
-        run('sudo ln -sf {} /data/web_static/current'.format(whereto))
+        run('rm -rf /data/web_static/current')
+        run('ln -sf {} /data/web_static/current'.format(whereto))
         print('New version deployed!')
     except:
         return False
-    else:
-        return True
+    return True
