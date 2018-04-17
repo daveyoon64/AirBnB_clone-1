a350145bdaf3cab34f3e3640fe19e529fdddb27f
Dave Yoon <daveyoon64@gmail.com>
2018-04-17 21:38:03 +0000

    First pass at ex 3

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""Starts Flask web application with python route"""
+from flask import Flask
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def hello_hbnb():
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb')
+def hbnb():
+    return 'HBNB'
+
+
+@app.route('/c/<text>')
+def c_text(text):
+    new = text.replace('_', ' ')
+    return 'C {}'.format(new)
+
+
+@app.route('/python')
+@app.route('/python/<text>')
+def python_text(text=None):
+    if not text:
+        text = 'is_cool'
+    new = text.replace('_', ' ')
+    return 'Python {}'.format(new)
+
+
+if __name__ == '__main__':
+    app.run(debug=False, host='0.0.0.0', port=5000)
