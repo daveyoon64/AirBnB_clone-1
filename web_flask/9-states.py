92a3a8b1b8fe294220e5c2d5d939138a2c585a4d
Dave Yoon <daveyoon64@gmail.com>
2018-04-19 05:22:50 +0000

    Combine reroutes into one

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -10,12 +10,6 @@ app.url_map.strict_slashes = False
 
 @app.route('/states')
 @app.route('/states/')
-def states_list():
-    """list all State objects present in DBStorage"""
-    states = storage.all('State').values()
-    return render_template('9-states.html', states=states)
-
-
 @app.route('/states/<id>')
 def states_id_list(id=None):
     """list all State objects present in DBStorage"""
@@ -23,12 +17,14 @@ def states_id_list(id=None):
     name = None
     states = storage.all('State').values()
     cities = storage.all('City').values()
+    if id is None:
+        return render_template('9-states.html', states=states)
     for state in states:
         if state.id == id:
             found_id = state.id
             name = state.name
-    if not id:
-        return return_template('9-states.html')
+    if found_id is None:
+        return render_template('9-states.html')
     else:
         return render_template('9-states.html',
                                id=found_id,
