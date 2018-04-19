17c1eaa7ebb40655d5e804685bb70982f260bc21
Dave Yoon <daveyoon64@gmail.com>
2018-04-19 03:56:45 +0000

    First pass at states reroute

---
+++ web_flask/9-states.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""starts the Flask server"""
+import sys
+from models import storage
+from flask import Flask, render_template
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/states')
+def states_list():
+    """list all State objects present in DBStorage"""
+    states = storage.all('State').values()
+    return render_template('9-states.html', states=states)
+
+
+@app.route('/states/<id>')
+def states_id_list(id):
+    """list all State objects present in DBStorage"""
+    found_id = None
+    name = None
+    states = storage.all('State').values()
+    cities = storage.all('City').values()
+    for state in states:
+        if state.id == id:
+            found_id = state.id
+            name = state.name
+    if not id:
+        return return_template('9-states.html', id=None)
+    else:
+        return render_template('9-states.html',
+                               id=found_id,
+                               name=name,
+                               cities=cities)
+
+
+@app.teardown_appcontext
+def remove_session(exception=None):
+    """remove current SQL Alcehmy session"""
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(debug=False, host='0.0.0.0', port=5000)
